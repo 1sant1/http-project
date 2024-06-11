e054749c94085d1a5d603c4552188a141d8c9da1
1sarso <125144275+1sarso@users.noreply.github.com>
2024-06-11 21:52:18 +0200

    Update serverLib.js
    Update serverLib.js to check for an API key in the headers

--- serverLib.js
+++ serverLib.js
@@ -1,6 +1,7 @@
 const net = require('net');
 
 const routes = {};
+const API_KEY = '012345';  // Sets the API key
 
 function on(method, path, handler) {
     routes[`${method}:${path}`] = handler;
@@ -12,25 +13,31 @@ function listen(port) {
             const [requestLine, ...headerLines] = data.toString().split('\r\n');
             const [method, path] = requestLine.split(' ');
 
+            const headers = {};
+            let body = '';
+            let isBody = false;
+
+            for (const line of headerLines) {
+                if (line === '') {
+                    isBody = true;
+                    continue;
+                }
+                if (isBody) {
+                    body += line;
+                } else {
+                    const [key, value] = line.split(': ');
+                    headers[key] = value;
+                }
+            }
+
+            if (headers['API-Key'] !== API_KEY) {
+                socket.write('HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n');
+                socket.end();
+                return;
+            }
+
             const handler = routes[`${method}:${path}`];
             if (handler) {
-                let headers = {};
-                let body = '';
-                let isBody = false;
-
-                for (const line of headerLines) {
-                    if (line === '') {
-                        isBody = true;
-                        continue;
-                    }
-                    if (isBody) {
-                        body += line;
-                    } else {
-                        const [key, value] = line.split(': ');
-                        headers[key] = value;
-                    }
-                }
-
                 handler({ method, path, headers, body }, {
                     send: (status, body) => {
                         socket.write(`HTTP/1.1 ${status}\r\nContent-Length: ${body.length}\r\n\r\n${body}`);
