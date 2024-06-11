15c99e46c99083f7d7392fe0fa562165c95154d6
1sarso <125144275+1sarso@users.noreply.github.com>
2024-06-11 22:04:59 +0200

    Update clientLib.js
    Ensured that the headers object is always initialized properly and that the API key is added correctly

--- clientLib.js
+++ clientLib.js
@@ -6,17 +6,16 @@ function parseURL(url) {
     return { hostname, port: port || 80, path: pathname };
 }
 
-function buildRequest({ method, path, headers, body }) {
+function buildRequest({ method, path, headers = {}, body }) {
     headers['API-Key'] = '12345';  // Add the API key
     const headerLines = Object.entries(headers).map(([key, value]) => `${key}: ${value}`).join('\r\n');
     return `${method} ${path} HTTP/1.1\r\n${headerLines}\r\n\r\n${body}`;
 }
 
-
 function request(method, url, headers = {}, body = '') {
     const { hostname, port, path } = parseURL(url);
     const client = new net.Socket();
-    const requestData = buildRequest(method, path, headers, body);
+    const requestData = buildRequest({ method, path, headers, body });
 
     return new Promise((resolve, reject) => {
         client.connect(port, hostname, () => {
@@ -36,5 +35,8 @@ function request(method, url, headers = {}, body = '') {
         client.on('error', reject);
     });
 }
+
+module.exports = { request };
+
 
 module.exports = { request };
